3917463b2bab1c5344cb5df96949bd2c172deb92
Cal Lees <caltriestocode@gmail.com>
2026-09-12 18:08:36 +0100

    create a concept to check if type is std::tuple

--- include/printN.h
+++ include/printN.h
@@ -2,7 +2,20 @@
 #include <cstddef>
 #include <iostream>
 #include <tuple>
+#include <type_traits>
 #include <utility>
+
+// when inheriting from std::false_type, is_tuple now has access to the ::value
+// member !
+template <typename T> struct is_tuple : std::false_type {};
+
+template <typename... T>
+struct is_tuple<std::tuple<T...>> : std::true_type {
+}; // partial specialisation on std:::tuple
+
+template <typename T>
+concept TupleType = is_tuple<T>::value;
+
 template <typename LAST> void printN(LAST t) { std::cout << t; }
 
 template <typename T0, typename... T> void printN(T0 t, T... args) {
@@ -11,11 +24,11 @@ template <typename T0, typename... T> void printN(T0 t, T... args) {
   printN(args...);
 }
 
-template <typename TUPLE, std::size_t... indices>
-void print_tuple_impl(TUPLE t, std::index_sequence<indices...>) {
+template <TupleType T, std::size_t... indices>
+void print_tuple_impl(T t, std::index_sequence<indices...>) {
   printN(std::get<indices>(t)...);
 }
 
-template <typename TUPLE> void print_tuple(TUPLE t) {
-  print_tuple_impl(t, std::make_index_sequence<std::tuple_size_v<TUPLE>>());
+template <TupleType T> void print_tuple(T t) {
+  print_tuple_impl(t, std::make_index_sequence<std::tuple_size_v<T>>());
 }
